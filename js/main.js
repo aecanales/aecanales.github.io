d02a32a98b267771009f3fbf9ad92bdeab2871ee
aecanales <aecanales@uc.cl>
2017-05-01 23:07:19 -0300

    Code cleaning
    Cleaned main.js and wrote readme

--- js/main.js
+++ js/main.js
@@ -1,38 +1,62 @@
-s = Snap("#svg");
-var database = [];
-var f_box = s.rect(0, 0, 200, 300, 10, 10);
-var f_name = s.text(10, 20, "test");
-var f_lifetime = s.text(10, 40, "1900-2000");
-var f_desc = s.text(10, 60, "Lorem Ipsum");
-f_box.attr({
-    fill: "#F7CDDE",
-    stroke: "#FFFFFF",
-    strokeWidth: 5
-});
+s = Snap("#svg"); //Main canvas
+var database = []; //Array with data from csv
 
-var f_text_box = s.group(f_box, f_name, f_lifetime, f_desc);
-f_text_box.attr({visibility: "hidden"});
+// SETTINGS - TIMELINE
+var left_edge = -130; //Snap.svg transfrom units (?)
+var bottom_edge = 50; //Snap.svg transfrom units (?)
 
-function moveFunc(ev, x, y ){
-	f_box.attr({x: x, y: y});
-	f_name.attr({x: x+10, y: y+20});
-	f_lifetime.attr({x: x+10, y: y+40});
-	f_desc.attr({x: x+10, y: y+60});	
-}
-s.mousemove(moveFunc);
+var bar_width = 4; // in n° of people
+var bar_spacing = 20; //Snap.svg transfrom units (?)
+
+var f_horizontal_spacing = 3; //Snap.svg transfrom units (?)
+var f_vertical_spacing = 6; //Snap.svg transfrom units (?)
+
+// SETTINGS - FEMALE DESCRIPTION TEXT BOX
+var f_text_dx = 10; //px
+var f_name_dy = 20; //px
+var f_lifetime_dy = 40; //px
+var f_desc_dy = 60; //px
+
+var f_box = {} //rect
+var f_name = {} //text
+var f_lifetime = {} //text
+var f_desc = {} //text, but... How do I do text wrap? D:
+var f_text_box = {} //group
+initializeFTextBox();
 
 window.onload = function() {
+	$( "#database" ).load( "data/database.csv", function() {
+		database = Papa.parse($('#database').val()).data;
+		drawTimeline(); //Timeline has to be drawn here to make sure the database has already been parsed
+	});
+}
 
-	var left_edge = -130;
-	var bottom_edge = 50;
+function initializeFTextBox(){
+	f_box = s.rect(0, 0, 200, 300, 10, 10);
+	f_name = s.text(f_text_dx, f_name_dy, "test");
+	f_lifetime = s.text(f_text_dx, f_lifetime_dy, "1900-2000");
+	f_desc = s.text(f_text_dx, f_desc_dy, "Lorem Ipsum");
+	f_box.attr({
+    	fill: "#F7CDDE",
+    	stroke: "#FFFFFF",
+    	strokeWidth: 5
+    });
+	f_text_box = s.group(f_box, f_name, f_lifetime, f_desc);
+	f_text_box.attr({visibility: "hidden"});
+	s.mousemove(fTextBoxFollowMouse);
+}
 
-	var bar_width = 4; // in n° of people
-	var bar_spacing = 20;
+function fTextBoxFollowMouse(ev, x, y ){
+	f_box.attr({x: x, y: y});
+	f_name.attr({x: x+f_text_dx, y: y+f_name_dy});
+	f_lifetime.attr({x: x+f_text_dx, y: y+f_lifetime_dy});
+	f_desc.attr({x: x+f_text_dx, y: y+f_desc_dy});
+	//Shouldn't I be able just to set the x and y of the f_text_box group? It hasn't worked the times I've tried it though...
+}
 
-	var f_horizontal_spacing = 3;
-	var f_vertical_spacing = 6;
-
-	// format [starting x, cur y, width counter] If there was some smarter way to define this that'd be cool...
+//TO DO - Comment how this code works
+function drawTimeline(){
+	// format: [starting x, cur y, width counter] If there was some smarter way to define this that'd be cool...
 	var years = [
 		[left_edge+bar_spacing*0, bottom_edge, 0], /*0 2008*/
 		[left_edge+bar_spacing*1, bottom_edge, 0], /*1 2009*/
@@ -46,21 +70,17 @@ window.onload = function() {
 		[left_edge+bar_spacing*9, bottom_edge, 0]  /*9 2017*/
 	];
 	
-	$( "#database" ).load( "data/database.csv", function() {
-		database = Papa.parse($('#database').val()).data;
-		//Code has to go here to make sure the database has downloaded
-		for (var i = 1; i < database.length; i++) {
-			if (database[i][0] == "") {continue;}
-			var year_i = getYearIndex(database[i][0]);
-			createFemale(years[year_i][0] + (f_horizontal_spacing*years[year_i][2]), years[year_i][1], database[i]);
-			years[year_i][2] += 1;
-			if (years[year_i][2] == bar_width) {
-				years[year_i][1] -= f_vertical_spacing;
-				years[year_i][2] = 0;
-			}
-		}
 
-	});
+	for (var i = 1; i < database.length; i++) {
+		if (database[i][0] == "") {continue;}
+		var year_i = getYearIndex(database[i][0]);
+		createFemale(years[year_i][0] + (f_horizontal_spacing*years[year_i][2]), years[year_i][1], database[i]);
+		years[year_i][2] += 1;
+		if (years[year_i][2] == bar_width) {
+			years[year_i][1] -= f_vertical_spacing;
+			years[year_i][2] = 0;
+		}
+	}
 }
 
 function getYearIndex(year){
@@ -74,7 +94,7 @@ function getYearIndex(year){
 	else if (year == "2015") {return 7}
 	else if (year == "2016") {return 8}
 	else if (year == "2017") {return 9}
-	else {return year}
+	else {return year} //TO DO - Check the database entries with no year!
 }
 
 function createFemale(x, y, info){
@@ -90,6 +110,7 @@ function createFemale(x, y, info){
 	});
 }
 
+//Takes an (x,y) translate and scale s and returns a string in the Snap.svg transfrom format
 function createTransfrom(x, y, s){
 	return "T"+x+","+y + " S"+s;
 }
@@ -105,14 +126,15 @@ function femaleHoverIn(){
 	f_text_box.appendTo(f_text_box.paper);
 }
 
-function femaleHoverOut(){
-	this.attr({fill:"#EA77A6"});
-	this.transform(createTransfrom(this.data("orig_t_x"), this.data("orig_t_y"), 0.05));
-	f_text_box.attr({visibility: "hidden"});
-}
-
+//TO DO - handle cases where female doesn't have death age
 function getLifetime(f_data){
 	death = parseInt(f_data[0]);
 	age = parseInt(f_data[5]);
 	return (death - age) + "-" + death
+}
+
+function femaleHoverOut(){
+	this.attr({fill:"#EA77A6"});
+	this.transform(createTransfrom(this.data("orig_t_x"), this.data("orig_t_y"), 0.05));
+	f_text_box.attr({visibility: "hidden"});
 }
---
+++ readme.txt
@@ -0,0 +1,12 @@
+https://aecanales.github.io/
+
+The following visualization is incomplete. Currently it:
+- Reads data from a database and draws a timeline based on the number of females
+- Mousing over a female gives you her description (but it currently doesn't work well because I don't know how to do text wrap D: )
+
+Female icon created by Mushu from the Noun Project: https://thenounproject.com/search/?q=female&creator=2482976&i=968330
+
+Javascript libraries used:
+- JQuery: For reading the database
+- PapaParse: For parsing the database
+- Snap.svg: SVG drawing and interaction
